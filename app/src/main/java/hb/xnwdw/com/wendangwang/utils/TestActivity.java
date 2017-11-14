034a336180e72c1dc405686ba2deecf3d022ddfb
南岸的风 <523833653@qq.com>
2017-11-14 11:58:16 +0800

    这次没问题吧

---
+++ app/src/main/java/hb/xnwdw/com/wendangwang/utils/TestActivity.java
@@ -0,0 +1,13 @@
+package hb.xnwdw.com.wendangwang.utils;
+
+/**
+ * Created by Administrator on 2017/11/14.
+ */
+
+public class TestActivity {
+
+    /***
+     *
+     *这是一个没用的activity
+     */
+}
